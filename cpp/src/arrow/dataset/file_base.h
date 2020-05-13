zhztheplayer/arrow

Datasets: Add SingleFileDataset

cpp/src/arrow/dataset/file_base.h
@@ -313,5 +313,37 @@ class ARROW_DS_EXPORT WritePlan {
   std::vector<std::string> paths;
 };
 
+class ARROW_DS_EXPORT SingleFileDataset : public Dataset {
+ public:
+  SingleFileDataset(std::shared_ptr<Schema> schema,
+                    std::shared_ptr<Expression> root_partition,
+                    std::shared_ptr<FileSource> file,
+                    std::shared_ptr<fs::FileSystem> fs,
+                    std::shared_ptr<FileFormat> format);
+
+
+  static Result<std::shared_ptr<SingleFileDataset>> Make(std::shared_ptr<Schema> schema,
+                                                         std::shared_ptr<Expression> root_partition,
+                                                         std::string path,
+                                                         std::shared_ptr<fs::FileSystem> fs,
+                                                         std::shared_ptr<FileFormat> format);
+
+  static Result<std::shared_ptr<SingleFileDataset>> Make(std::shared_ptr<Schema> schema,
+                                                         std::shared_ptr<Expression> root_partition,
+                                                         std::shared_ptr<FileSource> file,
+                                                         std::shared_ptr<fs::FileSystem> fs,
+                                                         std::shared_ptr<FileFormat> format);
+
+  std::string type_name() const override { return "single_file"; }
+
+ protected:
+  FragmentIterator GetFragmentsImpl(std::shared_ptr<ScanOptions> options) override ;
+
+ private:
+  std::shared_ptr<FileSource> file_;
+  std::shared_ptr<fs::FileSystem> fs_;
+  std::shared_ptr<FileFormat> format_;
+};
+
 }  // namespace dataset
 }  // namespace arrow

zhztheplayer/arrow-1

cpp/src/arrow/dataset/file_base.h
@@ -290,5 +290,38 @@ class ARROW_DS_EXPORT WritePlan {
   std::vector<std::string> paths;
 };
 
+class ARROW_DS_EXPORT SingleFileDataset : public Dataset {
+ public:
+  SingleFileDataset(std::shared_ptr<Schema> schema,
+                    std::shared_ptr<Expression> root_partition,
+                    std::shared_ptr<FileSource> file,
+                    std::shared_ptr<fs::FileSystem> fs,
+                    std::shared_ptr<FileFormat> format);
+
+
+  static Result<std::shared_ptr<SingleFileDataset>> Make(std::shared_ptr<Schema> schema,
+                                                         std::shared_ptr<Expression> root_partition,
+                                                         std::string path,
+                                                         std::shared_ptr<fs::FileSystem> fs,
+                                                         std::shared_ptr<FileFormat> format);
+
+  static Result<std::shared_ptr<SingleFileDataset>> Make(std::shared_ptr<Schema> schema,
+                                                         std::shared_ptr<Expression> root_partition,
+                                                         std::shared_ptr<FileSource> file,
+                                                         std::shared_ptr<fs::FileSystem> fs,
+                                                         std::shared_ptr<FileFormat> format);
+
+  Result<std::shared_ptr<Dataset>> ReplaceSchema(std::shared_ptr<Schema> schema) const override;
+  std::string type_name() const override { return "single_file"; }
+
+ protected:
+  FragmentIterator GetFragmentsImpl(std::shared_ptr<Expression> predicate) override;
+
+ private:
+  std::shared_ptr<FileSource> file_;
+  std::shared_ptr<fs::FileSystem> fs_;
+  std::shared_ptr<FileFormat> format_;
+};
+
 }  // namespace dataset
 }  // namespace arrow