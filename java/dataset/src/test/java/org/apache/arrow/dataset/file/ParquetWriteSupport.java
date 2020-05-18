3cfd488e0efeb27ea06fe944ae715f03fbd693db
Hongze Zhang <hongze.zhang@intel.com>
2020-05-18 14:26:55 +0800

    Untangle test codes

--- java/dataset/src/test/java/org/apache/arrow/dataset/file/ParquetWriteSupport.java
+++ java/dataset/src/test/java/org/apache/arrow/dataset/file/ParquetWriteSupport.java
@@ -20,9 +20,13 @@ package org.apache.arrow.dataset.file;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.arrow.util.Preconditions;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
@@ -35,13 +39,35 @@ class ParquetWriteSupport implements AutoCloseable {
   private final String path;
   private final ParquetWriter<GenericRecord> writer;
   private final Schema avroSchema;
+  private final List<GenericRecord> writtenRecords = new ArrayList<>();
+  private final GenericRecordListBuilder recordListBuilder = new GenericRecordListBuilder();
+
 
   public ParquetWriteSupport(String schemaName, File outputFolder) throws Exception {
-    avroSchema = getAvroSchema(schemaName);
+    avroSchema = readSchemaFromFile(schemaName);
     path = outputFolder.getPath() + File.separator + "generated.parquet";
     writer = AvroParquetWriter.<GenericRecord>builder(new org.apache.hadoop.fs.Path(path))
         .withSchema(avroSchema)
         .build();
+  }
+
+  private static Schema readSchemaFromFile(String schemaName) throws Exception {
+    Path schemaPath = Paths.get(ParquetWriteSupport.class.getResource("/").getPath(),
+        "avroschema", schemaName);
+    return new org.apache.avro.Schema.Parser().parse(schemaPath.toFile());
+  }
+
+  public static ParquetWriteSupport writeTempFile(String schemaName, File outputFolder,
+      Object... values) throws Exception {
+    try (final ParquetWriteSupport writeSupport = new ParquetWriteSupport(schemaName, outputFolder)) {
+      writeSupport.writeRecords(values);
+      return writeSupport;
+    }
+  }
+
+  public void writeRecords(Object... values) throws Exception {
+    final List<GenericRecord> valueList = getRecordListBuilder().createRecordList(values);
+    writeRecords(valueList);
   }
 
   public void writeRecords(List<GenericRecord> records) throws Exception {
@@ -51,6 +77,7 @@ class ParquetWriteSupport implements AutoCloseable {
   }
 
   public void writeRecord(GenericRecord record) throws Exception {
+    writtenRecords.add(record);
     writer.write(record);
   }
 
@@ -62,14 +89,33 @@ class ParquetWriteSupport implements AutoCloseable {
     return avroSchema;
   }
 
-  private org.apache.avro.Schema getAvroSchema(String schemaName) throws Exception {
-    Path schemaPath = Paths.get(ParquetWriteSupport.class.getResource("/").getPath(),
-        "avroschema", schemaName);
-    return new org.apache.avro.Schema.Parser().parse(schemaPath.toFile());
+  public GenericRecordListBuilder getRecordListBuilder() {
+    return recordListBuilder;
+  }
+
+  public List<GenericRecord> getWrittenRecords() {
+    return Collections.unmodifiableList(writtenRecords);
   }
 
   @Override
   public void close() throws Exception {
     writer.close();
+  }
+
+  public class GenericRecordListBuilder {
+    public final List<GenericRecord> createRecordList(Object... values) {
+      final int fieldCount = avroSchema.getFields().size();
+      Preconditions.checkArgument(values.length % fieldCount == 0,
+          "arg count of values should be divide by field number");
+      final List<GenericRecord> recordList = new ArrayList<>();
+      for (int i = 0; i < values.length / fieldCount; i++) {
+        final GenericRecord record = new GenericData.Record(avroSchema);
+        for (int j = 0; j < fieldCount; j++) {
+          record.put(j, values[i * fieldCount + j]);
+        }
+        recordList.add(record);
+      }
+      return Collections.unmodifiableList(recordList);
+    }
   }
 }
--- java/dataset/src/test/java/org/apache/arrow/dataset/file/TestSingleFileDataset.java
+++ java/dataset/src/test/java/org/apache/arrow/dataset/file/TestSingleFileDataset.java
@@ -37,7 +37,6 @@ import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
 import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.junit.ClassRule;
@@ -55,83 +54,57 @@ public class TestSingleFileDataset extends TestNativeDataset {
 
   @Test
   public void testParquetRead() throws Exception {
-    ParquetWriteSupport writeSupport = new ParquetWriteSupport(AVRO_SCHEMA_USER, TMP.newFolder());
-    GenericRecord record = new GenericData.Record(writeSupport.getAvroSchema());
-    record.put("id", 1);
-    record.put("name", "a");
-    writeSupport.writeRecord(record);
-    writeSupport.close();
+    ParquetWriteSupport writeSupport = ParquetWriteSupport.writeTempFile(AVRO_SCHEMA_USER, TMP.newFolder(), 1, "a");
 
     SingleFileDatasetFactory factory = new SingleFileDatasetFactory(rootAllocator(),
         FileFormat.PARQUET, FileSystem.LOCAL, writeSupport.getOutputFile());
     ScanOptions options = new ScanOptions(new String[0], Filter.EMPTY, 100);
-
     Schema schema = inferResultSchemaFromFactory(factory, options);
+    List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
+
+    assertSingleTaskProduced(factory, options);
+    assertEquals(1, datum.size());
     assertEquals(2, schema.getFields().size());
     assertEquals("id", schema.getFields().get(0).getName());
     assertEquals("name", schema.getFields().get(1).getName());
     assertEquals(Types.MinorType.INT.getType(), schema.getFields().get(0).getType());
     assertEquals(Types.MinorType.VARCHAR.getType(), schema.getFields().get(1).getType());
+    checkParquetReadResult(schema, writeSupport.getWrittenRecords(), datum);
 
-    assertSingleTaskProduced(factory, options);
-
-    List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
-    assertEquals(1, datum.size());
-    checkParquetReadResult(schema, Collections.singletonList(record), datum);
     AutoCloseables.close(datum);
   }
 
   @Test
   public void testParquetProjector() throws Exception {
-    ParquetWriteSupport writeSupport = new ParquetWriteSupport(AVRO_SCHEMA_USER, TMP.newFolder());
-    GenericRecord record = new GenericData.Record(writeSupport.getAvroSchema());
-    record.put("id", 1);
-    record.put("name", "a");
-    writeSupport.writeRecord(record);
-    writeSupport.close();
+    ParquetWriteSupport writeSupport = ParquetWriteSupport.writeTempFile(AVRO_SCHEMA_USER, TMP.newFolder(), 1, "a");
 
     SingleFileDatasetFactory factory = new SingleFileDatasetFactory(rootAllocator(),
         FileFormat.PARQUET, FileSystem.LOCAL, writeSupport.getOutputFile());
     ScanOptions options = new ScanOptions(new String[]{"id"}, Filter.EMPTY, 100);
-
     Schema schema = inferResultSchemaFromFactory(factory, options);
+    List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
+    org.apache.avro.Schema expectedSchema = truncateAvroSchema(writeSupport.getAvroSchema(), 0, 1);
+
+    assertSingleTaskProduced(factory, options);
     assertEquals(1, schema.getFields().size());
     assertEquals("id", schema.getFields().get(0).getName());
     assertEquals(Types.MinorType.INT.getType(), schema.getFields().get(0).getType());
-
-    assertSingleTaskProduced(factory, options);
-
-    List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
     assertEquals(1, datum.size());
-    org.apache.avro.Schema expectedSchema = truncateAvroSchema(writeSupport.getAvroSchema(), 0, 1);
     checkParquetReadResult(schema,
         Collections.singletonList(
             new GenericRecordBuilder(
                 expectedSchema)
                 .set("id", 1)
                 .build()), datum);
+
     AutoCloseables.close(datum);
   }
 
   @Test
   public void testParquetFilter() throws Exception {
-    ParquetWriteSupport writeSupport = new ParquetWriteSupport(AVRO_SCHEMA_USER, TMP.newFolder());
-    List<GenericRecord> records = newGenericRecordListBuilder()
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 1)
-            .set("name", "a")
-            .build())
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 2)
-            .set("name", "b")
-            .build())
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 3)
-            .set("name", "c")
-            .build())
-        .build();
-    writeSupport.writeRecords(records);
-    writeSupport.close();
+    ParquetWriteSupport writeSupport = ParquetWriteSupport.writeTempFile(AVRO_SCHEMA_USER, TMP.newFolder(),
+        1, "a", 2, "b", 3, "c");
+
     SingleFileDatasetFactory factory = new SingleFileDatasetFactory(rootAllocator(),
         FileFormat.PARQUET, FileSystem.LOCAL, writeSupport.getOutputFile());
     Filter filter = new Filter(DatasetTypes.Condition.newBuilder()
@@ -149,44 +122,31 @@ public class TestSingleFileDataset extends TestNativeDataset {
         .build());
     ScanOptions options = new ScanOptions(new String[0], filter, 100);
     Schema schema = inferResultSchemaFromFactory(factory, options);
-    assertSingleTaskProduced(factory, options);
     List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
+
+    assertSingleTaskProduced(factory, options);
     assertEquals(1, datum.size());
-    GenericRecord record = new GenericData.Record(writeSupport.getAvroSchema());
-    record.put("id", 2);
-    record.put("name", "b");
-    checkParquetReadResult(schema, Collections.singletonList(record), datum);
+    checkParquetReadResult(schema, writeSupport.getRecordListBuilder().createRecordList(2, "b"), datum);
+
     AutoCloseables.close(datum);
   }
 
   @Test
   public void testParquetBatchSize() throws Exception {
-    ParquetWriteSupport writeSupport = new ParquetWriteSupport(AVRO_SCHEMA_USER, TMP.newFolder());
-    List<GenericRecord> records = newGenericRecordListBuilder()
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 1)
-            .set("name", "a")
-            .build())
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 2)
-            .set("name", "b")
-            .build())
-        .add(new GenericRecordBuilder(writeSupport.getAvroSchema())
-            .set("id", 3)
-            .set("name", "c")
-            .build())
-        .build();
-    writeSupport.writeRecords(records);
-    writeSupport.close();
+    ParquetWriteSupport writeSupport = ParquetWriteSupport.writeTempFile(AVRO_SCHEMA_USER, TMP.newFolder(),
+        1, "a", 2, "b", 3, "c");
+
     ScanOptions options = new ScanOptions(new String[0], Filter.EMPTY, 1);
     SingleFileDatasetFactory factory = new SingleFileDatasetFactory(rootAllocator(),
         FileFormat.PARQUET, FileSystem.LOCAL, writeSupport.getOutputFile());
     Schema schema = inferResultSchemaFromFactory(factory, options);
-    assertSingleTaskProduced(factory, options);
     List<ArrowRecordBatch> datum = collectResultFromFactory(factory, options);
+
+    assertSingleTaskProduced(factory, options);
     assertEquals(3, datum.size());
     datum.forEach(batch -> assertEquals(1, batch.getLength()));
-    checkParquetReadResult(schema, records, datum);
+    checkParquetReadResult(schema, writeSupport.getWrittenRecords(), datum);
+
     AutoCloseables.close(datum);
   }
 
